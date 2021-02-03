76026f9d6f529e5faf0b1a5b023cf0c7b8934cb5
Ricardo <ricardo.agnello@bol.com.br>
2021-02-02 21:54:44 -0300

    project created

---
+++ motoristaApp/src/test/java/com/neutronstar/motoristaapp/MotoristaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutronstar.motoristaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoristaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
